e0bb62336e83155a26d745addc6987436a5f2fe6
Larry Marburger <larry@marburger.cc>
2018-02-26 12:41:55 -0500

    Unmap inconvenient row of keys to unlearn bad habits

--- keyboards/kinesis/keymaps/larry/keymap.c
+++ keyboards/kinesis/keymaps/larry/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |--------+------+------+------+------+------|
 * | Shift  |   Z  |   X  |   C  |   V  |   B  |
 * `--------+------+------+------+------+-------
-*          | [{   | ]}   | Left | Right|
+*          |      |      |      |      |
 *          `---------------------------'
 *                                        ,-------------.
 *                                        | Gui  | Alt  |
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_EQL, KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
   KC_TAB, KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,
   KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,
-          KC_LBRC,KC_RBRC,KC_LEFT,KC_RGHT,
+          _______,_______,_______,_______,
                                           KC_LGUI,KC_LALT,
                                                   OMG    ,
                                   KC_BSPC,KC_ESC ,KC_LCTL,
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 *                      |------+------+------+------+------+--------|
 *                      |   N  |   M  |  ,<  |  .>  |  /?  | Shift  |
 *                      `------+------+------+------+------+--------'
-*                             | Down | Up   |  [{  |  ]}  |
+*                             |      |      |      |      |
 *                             `---------------------------'
 *             ,-------------.
 *             | Alt  | Gui  |
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSLS,
                           KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,
                           KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
-                                  KC_DOWN,KC_UP  ,KC_LBRC,KC_RBRC,
+                                  _______,_______,_______,_______,
                           KC_RALT,KC_RGUI,
                           WTF    ,
                           KC_RCTL,KC_ENTER ,KC_SPC
